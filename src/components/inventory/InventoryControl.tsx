7b68d312e772169f35798770a7acb3ce0c8917f8
b34roff <gumimacihun@gmail.com>
2024-11-20 22:18:13 +0100

    fix : fixed display none on command button and input

--- src/components/inventory/InventoryControl.tsx
+++ src/components/inventory/InventoryControl.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { useDrop } from 'react-dnd';
+import { onGive } from '../../dnd/onGive';
+import { onUse } from '../../dnd/onUse';
 import { useAppDispatch, useAppSelector } from '../../store';
 import { selectItemAmount, setItemAmount } from '../../store/inventory';
-import { DragSource } from '../../typings';
-import { onUse } from '../../dnd/onUse';
-import { onGive } from '../../dnd/onGive';
-import { fetchNui } from '../../utils/fetchNui';
 import { Locale } from '../../store/locale';
+import { DragSource } from '../../typings';
 import UsefulControls from './UsefulControls';
 
 const InventoryControl: React.FC = () => {
@@ -40,6 +39,9 @@ const InventoryControl: React.FC = () => {
       <UsefulControls infoVisible={infoVisible} setInfoVisible={setInfoVisible} />
       <div className="inventory-control">
         <div className="inventory-control-wrapper">
+          <button className="inventory-control-button" ref={give}>
+            {Locale.ui_give || 'Give'}
+          </button>
           <input
             className="inventory-control-input"
             type="number"
@@ -50,20 +52,17 @@ const InventoryControl: React.FC = () => {
           <button className="inventory-control-button" ref={use}>
             {Locale.ui_use || 'Use'}
           </button>
-          <button className="inventory-control-button" ref={give}>
-            {Locale.ui_give || 'Give'}
-          </button>
-          <button className="inventory-control-button" onClick={() => fetchNui('exit')}>
+          {/* <button className="inventory-control-button" onClick={() => fetchNui('exit')}>
             {Locale.ui_close || 'Close'}
-          </button>
+          </button> */}
         </div>
       </div>
 
-      <button className="useful-controls-button" onClick={() => setInfoVisible(true)}>
+      {/* <button className="useful-controls-button" onClick={() => setInfoVisible(true)}>
         <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 524 524">
           <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
         </svg>
-      </button>
+      </button> */}
     </>
   );
 };
